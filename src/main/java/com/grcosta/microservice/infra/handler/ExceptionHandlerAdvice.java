1321c49b5e6811021ea9554515222bab4d64db6e
GRMCosta <rafa_gian2011@hotmail.com>
2020-01-28 22:57:56 -0300

    Add my dependency to my microservice

---
+++ src/main/java/com/grcosta/microservice/infra/handler/ExceptionHandlerAdvice.java
@@ -0,0 +1,15 @@
+package com.grcosta.microservice.infra.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.grcosta.messagelocator.interfaces.MessageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionHandlerAdvice {
+
+
+
+}
