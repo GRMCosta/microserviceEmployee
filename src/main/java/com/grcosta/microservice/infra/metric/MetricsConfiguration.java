b1b5255ae20e8ffde6a98e166eba9ebc0e87af0a
GRMCosta <rafa_gian2011@hotmail.com>
2020-01-30 19:00:04 -0300

    Folder Handle and Metric

---
+++ src/main/java/com/grcosta/microservice/infra/metric/MetricsConfiguration.java
@@ -0,0 +1,28 @@
+package com.grcosta.microservice.infra.metric;
+
+import com.grcosta.messagelocator.interfaces.MessageService;
+import com.grcosta.messagelocator.service.EnvironmentMessageService;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class MetricsConfiguration {
+
+  @Bean
+  MeterRegistryCustomizer<MeterRegistry> registerCommonTags(Environment environment){
+    final String applicationName = environment.getProperty("spring.application.name");
+    return registry -> registry.config().commonTags(
+        "app_name", applicationName,
+        "env", environment.getActiveProfiles()[0])
+        .namingConvention(new MetricsNamingConvention(applicationName));
+  }
+
+  @Bean
+  public MessageService createMessageService(MeterRegistry meterRegistry) {
+    return new EnvironmentMessageService(meterRegistry);
+  }
+
+}
